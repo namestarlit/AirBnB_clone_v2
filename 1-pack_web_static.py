5eca307cdf1edaab042dd70dc35fcc8f63015547
ifouzezi <ifoghaleuzezi@gmail.com>
2023-07-05 21:49:42 +0100

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,7 +6,7 @@ from fabric.api import local
 
 
 def do_pack():
-    """Create a tar gzipped archive of the directory web_static."""
+    """Create a tar gzipped archive of the web_static folder."""
     dt = datetime.utcnow()
     file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
                                                          dt.month,
@@ -14,9 +14,16 @@ def do_pack():
                                                          dt.hour,
                                                          dt.minute,
                                                          dt.second)
-    if os.path.isdir("versions") is False:
-        if local("mkdir -p versions").failed is True:
-            return None
-    if local("tar -cvzf {} web_static".format(file)).failed is True:
+    if os.path.isdir("web_static") is False:
+        print("Error: web_static folder not found.")
         return None
+
+    if os.path.isdir("versions") is False:
+        os.makedirs("versions")
+
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
+        print("Error: Failed to create the archive.")
+        return None
+
+    print("Archive created: {}".format(file))
     return file
