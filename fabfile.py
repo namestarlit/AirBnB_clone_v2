2f9df41615239d00b4fc66acf67d2610f3aea143
ifouzezi <ifoghaleuzezi@gmail.com>
2023-07-05 21:55:46 +0100

    Update fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,25 @@
 from fabric import task
+from datetime import datetime
+import os
+import shutil
 
 @task
 def do_pack():
-    # Task implementation here
-    pass
+    """Create a tar gzipped archive of the web_static folder."""
+    now = datetime.now()
+    timestamp = now.strftime("%Y%m%d%H%M%S")
+    archive_name = f"web_static_{timestamp}.tgz"
+    archive_path = f"versions/{archive_name}"
 
-# Define more tasks if needed
+    # Create the versions directory if it doesn't exist
+    os.makedirs("versions", exist_ok=True)
+
+    try:
+        shutil.make_archive("web_static", "gztar", "web_static")
+        shutil.move("web_static.tar.gz", archive_path)
+        print(f"Archive created: {archive_path}")
+        return archive_path
+    except Exception as e:
+        print(f"Error creating archive: {e}")
+        return None
+
