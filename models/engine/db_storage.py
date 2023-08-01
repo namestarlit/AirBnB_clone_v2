ec19391dba25c9c205d147285a996f5901910ece
Paul John <rainson.work@gmail.com>
2023-08-01 22:00:22 +0300

    Add `db_storage.py`

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-"""This is the file storage class for AirBnB"""
+"""Database Storage implementantion.
+
+Implementation of database operations using MySQL Database.
+"""
+
 from os import getenv
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
-from models.base_model import BaseModel, Base
+
+from models.base_model import Base
 from models.user import User
 from models.state import State
 from models.city import City
@@ -12,61 +18,63 @@ from models.place import Place
 from models.review import Review
 
 
+def import_models():
+    """Dynamic import of models."""
+    return {
+        "User": User,
+        "State": State,
+        "City": City,
+        "Amenity": Amenity,
+        "Place": Place,
+        "Review": Review
+    }
+
+
 class DBStorage:
-    """
-    Database Engine
-    """
+    """Database Engine."""
     __engine = None
     __session = None
-    models = {User, State, City}
+    model_mapping = import_models()
 
     def __init__(self):
-        """
-        Starts the DB engine
-        """
+        """Starts the DB engine."""
         user = getenv("HBNB_MYSQL_USER")
         password = getenv("HBNB_MYSQL_PWD")
         host = getenv("HBNB_MYSQL_HOST")
         db = getenv("HBNB_MYSQL_DB")
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
-            user, password, host, db), pool_pre_ping=True)
+        db_url = ("mysql+mysqldb://{}:{}@{}/{}"
+                  .format(user, password, host, db)
+                  )
+        self.__engine = create_engine(db_url, pool_pre_ping=True)
         if getenv("HBNB_ENV") == "test":
             Base.metadata.drop_all(bind=self.__engine)
 
     def all(self, cls=None):
-        """
-        returns a dictionary
-        """
+        """Returns a dictionary."""
         sql_dict = {}
-        models = self.models
+        models = list(self.model_mapping.values())
         if cls:
-            models = {cls}
+            models = [self.model_mapping[cls.__name__]]
         for model in models:
-            print(type(model))
-            objects = self.__session.query(eval(model)).all()
-            print(objects)
+            objects = self.__session.query(model).all()
             for obj in objects:
                 key = "{}.{}".format(type(obj).__name__, obj.id)
                 sql_dict[key] = obj
         return sql_dict
 
     def new(self, obj):
-        """
-        add the object to the current database session
-        """
+        """Adds the object to the current database session."""
         if obj:
             self.__session.add(obj)
 
     def save(self):
-        """
-        commit all changes of the current database session
-        """
+        """Commits all changes of the current database session."""
         self.__session.commit()
 
     def reload(self):
-        """
-        create all tables in the database
-        create the current database scoped session
+        """ Creates all tables in the database.
+
+        Creates the current database scoped session
         """
         Base.metadata.create_all(self.__engine)
         Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
@@ -74,14 +82,10 @@ class DBStorage:
         self.__session = Session()
 
     def delete(self, obj=None):
-        """
-        delete from the current database session
-        """
+        """Deletes from the current database session."""
         if obj is not None:
             self.__session.delete(obj)
 
     def close(self):
-        """
-        call remove() method on the private session attribute (self.__session)
-        """
+        """Calls remove() method on the private session attribute."""
         self.__session.close()
