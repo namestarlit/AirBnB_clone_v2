e75c53fc05236a8b5bd9247f395e36bb91973391
Paul John <rainson.work@gmail.com>
2023-08-01 12:22:52 +0300

    Modify `models/amenity.py`

--- models/amenity.py
+++ models/amenity.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
-"""Holds class Amenity"""
+"""Module defines the Amenity class."""
+
+from os import getenv
+
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlachmemy.orm import relationship
+
 import models
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """Representation of Amenity"""
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
+    """Represents an amenity."""
+
+    if getenv('HBNB_TYPE_STORAGE') == "db":
         __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
     else:
         name = ""
 
     def __init__(self, *args, **kwargs):
-        """Initializes Amenity"""
+        """Initializes a new Amenity instance."""
         super().__init__(*args, **kwargs)
