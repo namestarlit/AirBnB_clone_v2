c004213dbbf0f9be36cc2ad337a1c5faff9cc2a7
Paul John <rainson.work@gmail.com>
2023-08-01 21:59:43 +0300

    Add `user.py`

--- models/user.py
+++ models/user.py
@@ -1,31 +1,35 @@
 #!/usr/bin/python3
-""" holds class User"""
+"""Module contains User class."""
+
 import hashlib
-import models
-from models.base_model import BaseModel, Base
 from os import getenv
+
+from sqlalchemy import Column
+from sqlalchemy import String
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String
+
+from models.base_model import BaseModel, Base
 
 
 class User(BaseModel, Base):
-    """Representation of a user"""
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'users'
+    """Represents a User class."""
+    if getenv('HBNB_TYPE_STORAGE') == "db":
+        __tablename__ = "users"
         email = Column(String(128), nullable=False)
-        password = Column('password', String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user", cascade="all, delete-orphan")
-        reviews = relationship("Review", backref="user", cascade="all, delete-orphan")
+        _password = Column('password', String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+        places = relationship('Place', backref='user',
+                              cascade='all, delete-orphan')
+        reviews = relationship('Review', backref='user',
+                               cascade='all, delete-orphan')
     else:
         email = ""
-        password = ""
+        _password = ""
         first_name = ""
         last_name = ""
 
     def __init__(self, *args, **kwargs):
-        """initializes user"""
         super().__init__(*args, **kwargs)
 
     @property
@@ -35,4 +39,4 @@ class User(BaseModel, Base):
     @password.setter
     def password(self, pwd):
         """hashing password values"""
-        self._password = hashlib.md5(pwd.encode()).hexdigest()
+        self._password = hashlib.sha256(pwd.encode()).hexdigest()
