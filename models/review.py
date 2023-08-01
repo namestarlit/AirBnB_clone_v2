497617032f06e9ea38fe5ff1a8a5e56945a3385d
Paul John <rainson.work@gmail.com>
2023-08-01 21:59:09 +0300

    Add `review.py`

--- models/review.py
+++ models/review.py
@@ -1,24 +1,27 @@
-#!/usr/bin/python
-""" holds class Review"""
-import models
-from models.base_model import BaseModel, Base
+#!/usr/bin/python3
+"""Module containing a Review class."""
+
 from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
+
+from sqlalchemy import Column
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import relationship
+
+from models.base_model import BaseModel, Base
 
 
 class Review(BaseModel, Base):
-    """Representation of Review """
-    if getenv("HBNB_TYPE_STORAGE") == 'db':
-        __tablename__ = 'reviews'
-        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-        text = Column(String(1024), nullable=False)
+    """Represents a Review class."""
+    if getenv('HBNB_TYPE_STORAGE') == "db":
+        __tablename__ = "reviews"
+        place_id = Column(String(60), ForeignKey('places.id'))
+        user_id = Column(String(60), ForeignKey('users.id'))
+        text = Column(String(128), nullable=False)
     else:
         place_id = ""
         user_id = ""
         text = ""
 
     def __init__(self, *args, **kwargs):
-        """initializes Review"""
+        """Initializes an instance of Review."""
         super().__init__(*args, **kwargs)
