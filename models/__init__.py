70c414ccdf8fa629c0aac1ba44d37096fbae5f32
Paul John <rainson.work@gmail.com>
2023-08-01 12:04:38 +0300

    Modify `models/__init__.py`

--- models/__init__.py
+++ models/__init__.py
@@ -9,7 +9,9 @@ Otherwise, instantiates a file storage engine (FileStorage).
 from os import getenv
 
 
-if getenv('HBNB_TYPE_STORAGE') == "db":
+storage_type = getenv('HBNB_TYPE_STORAGE')
+
+if storage_type == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
